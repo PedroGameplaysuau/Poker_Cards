f747c7a29b3551bf44edadf52c4c27d054f37f88
PedroGameplaysuau <133403074+PedroGameplaysuau@users.noreply.github.com>
2024-09-22 16:14:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'charadas idiotas',
+    'Por que os pássaros voam para o sul?',
+    ' porque andando levariam muito tempo'
+)
+
+criaCartao(
+    'charadas idiotas',
+    'Por que o dinossauro não consegue bater palma?',
+    'porque ele já foi extinto.'
+)
+
+criaCartao(
+    'charadas idiotas',
+    'Se um astronauta mata outro na Lua ele pode ser preso?',
+    'não, porque é um crime de pouca gravidade'
+)
+
+criaCartao(
+    'charadas idiotas',
+    'Sabe por que nunca se deve desistir dos sonhos?',
+    ' porque se não tiver na padaria mais próxima pode ser que tenha na outra.'
+)
